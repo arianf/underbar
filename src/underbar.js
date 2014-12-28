50a038e7414ed38925d91333e1b1f684fcc978af
Arian Faurtosh <arian@icloud.com>
2014-12-28 00:09:28 -0800

    Finished last function

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,15 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if(n === undefined){
+      return array[array.length-1];
+    }else if( n > array.length ){
+      return array;
+    }else if( n === 0){
+      return [];
+    }else{
+      return array.slice(n-1, array.length+1);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.

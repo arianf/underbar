598452dee5e405fda2348b4674c1909b90563ba2
Arian Faurtosh <arian@icloud.com>
2014-12-28 01:01:01 -0800

    Fixed the _.filter function... kind of complicated

--- src/underbar.js
+++ src/underbar.js
@@ -85,6 +85,12 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var array = [];
+    _.each(collection, function(item) {
+      if(test(item))
+        array.push(item);
+    });
+    return array;
   };
 
   // Return all elements of an array that don't pass a truth test.

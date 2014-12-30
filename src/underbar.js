73be5fa8875d8c3014617b7cc4fcbbfb4e87adab
Arian Faurtosh <arian@icloud.com>
2014-12-29 17:32:59 -0800

    Okay, so the if statement is extemely ugly... i'm sorry, I am trying to think of a way to remove it or make it prettier. it just sucks that I have to check accumulator twice

--- src/underbar.js
+++ src/underbar.js
@@ -165,6 +165,13 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    var total = (accumulator === undefined ? collection[0] : accumulator);
+    _.each(collection, function(item, key){
+      if(!(accumulator === undefined && key === 0))
+        total = iterator(total, item);
+    });
+
+    return total;
   };
 
   // Determine if the array or object contains a given value (using `===`).
